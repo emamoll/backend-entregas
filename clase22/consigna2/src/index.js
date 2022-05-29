1df480198f52d279ec7d519c40f35a76d8d9f911
Emanuel <emanuelgmoll@gmail.com>
2022-05-29 16:05:51 -0300

    Clase 22

--- clase22/consigna2/src/index.js
+++ clase22/consigna2/src/index.js
@@ -1,11 +1,1 @@
-import Server from './services/server';
-import { initWsServer } from './services/socket';
-
-const puerto = 8080;
-
-const init = async () => {
-    const io = initWsServer(Server);
-    Server.listen(puerto, () => console.log(`Escuchando en ${puerto}`));
-};
-
-init();
+import { config } from "dotenv";
